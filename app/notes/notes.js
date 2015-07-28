f5a6da93e0af0a73d96d4735d9178ea5056a479a
Dave Strus <dave@davestrus.com>
2015-07-28 15:40:53 -0400

    Load notes via Nevernote API.

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var notelyBasePath = 'https://nevernote-1150.herokuapp.com/api/v1/';
+var apiKey = '$2a$10$TTc8gLTzfWBk9SsDO7p.J.acOzMMG535814CudrCMQgmjUSvbQ2ju';
+
 angular.module('notely.notes', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -9,6 +12,9 @@ angular.module('notely.notes', ['ngRoute'])
   });
 }])
 
-.controller('NotesController', [function() {
-
-}]);
+.controller('NotesController', function NotesController($scope, $http) {
+  $http.get(notelyBasePath + 'notes?api_key=' + apiKey)
+    .success(function(notes_data) {
+      $scope.notes = notes_data;
+    });
+});
